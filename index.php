5be1ce3a73b09d09dc4be17e034daf223e2bfbbf
justnarrator <fivezjd@outlook.com>
2016-11-23 00:00:57 +0800

    添加主干文件

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 张建东
+ * Date: 2016/11/22
+ * Time: 22:20
+ */
+$version = (string)phpversion();
+$parseNum = $version[0].$version[2];
+if((int)$parseNum < 55){
+    die("请升级PHP版本");
+}
+define("DEBUG",TRUE);
